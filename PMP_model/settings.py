5c59bf58a7e9287cf5b057fef24e06591fef28e9
Shiv0063 <vishalba288@gmail.com>
2023-06-19 16:01:30 +0530

    new change

--- PMP_model/settings.py
+++ PMP_model/settings.py
@@ -36,7 +36,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = "django-insecure-k%@omsn(y)d%c2c3_c*d2(ajx2(i4cbtp)42hf-4_e^r41)&(%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
