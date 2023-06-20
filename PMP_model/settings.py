54ba2acf06966bcc77d5570d7d960dc6f6dae947
Shiv0063 <vishalba288@gmail.com>
2023-06-20 13:49:56 +0530

    statik

--- PMP_model/settings.py
+++ PMP_model/settings.py
@@ -24,7 +24,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-STATICFILES_DIR = [STATIC_DIR]
+STATICFILES_DIRS=[
+STATIC_DIR,
+ ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -134,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
