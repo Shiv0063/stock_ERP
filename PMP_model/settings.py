0879a7bdf7ec51e6209e78f0cb2cd63c5505647a
Shiv0063 <vishalba288@gmail.com>
2023-06-20 12:41:37 +0530

    change

--- PMP_model/settings.py
+++ PMP_model/settings.py
@@ -25,7 +25,6 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
